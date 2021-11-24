601f308e2b5681a62076be716a701f76beb6ce96
prokopolja <kotejechka@gmail.com>
2021-11-24 13:17:21 +0300

    добавила карту через api

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
+ymaps.ready(init);
+
+function init(){
+  var myMap = new ymaps.Map("map", {
+    center: [7.890749861015384,98.29471855302582],
+    zoom: 17,
+  })
+}
 const swiper = new Swiper('.swiper', {
   // Optional parameters
   loop: true,
